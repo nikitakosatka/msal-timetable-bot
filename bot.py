bfa2c11a6bcd4456dc44b84de2a2ce2d4a7c0ed8
nikitakosatka <nikitakosatka@yandex.ru>
2021-10-12 20:28:33 +0300

    /start и /help

--- bot.py
+++ bot.py
@@ -11,6 +11,17 @@ bot = telebot.TeleBot(token)
 weeks = get_weeks()
 
 
+@bot.message_handler(commands=["start", "help"])
+def send_help(message):
+    text = "Привет! Это бот с расписанием МГЮА\n" \
+           "Команды:\n" \
+           "\"Сегодня\" - расписание на сегодняшний день\n" \
+           "\"Завтра\" - расписание на завтра\n" \
+           "\"MM.DD\" - раписание на указанную дату, например \"12.10\"\n" \
+           "\"День недели\" - расписание на указанный день недели (в текущей неделе), например \"Вторник\"\n"
+    bot.send_message(message.chat.id, text)
+
+
 @bot.message_handler(content_types=['text'])
 def send_timetable(message):
     today = date.today()

6d81f90d828f52c7ffb3db5d87c770753df933db
nikitakosatka <nikitakosatka@yandex.ru>
2021-10-12 19:53:57 +0300

    Получение расписание на сегодня и завтра

--- bot.py
+++ bot.py
@@ -1,9 +1,9 @@
 import telebot
 import logging
-from datetime import date
+from datetime import date, timedelta
 
 from key import token
-from excel_parser import get_weeks, get_day_by_date
+from excel_parser import *
 
 # logging.basicConfig(level=logging.DEBUG)
 bot = telebot.TeleBot(token)
@@ -11,14 +11,13 @@ bot = telebot.TeleBot(token)
 weeks = get_weeks()
 
 
-@bot.message_handler(commands=['start'])
-def send_welcome(message):
-    logging.debug("Welcome")
-    bot.send_message(message.chat.id, "Hello, World!")
-
-
 @bot.message_handler(content_types=['text'])
 def send_timetable(message):
-    today = date.today().strftime("%d.%m")
-    timetable = get_day_by_date(weeks, today)
-    bot.send_message(message.chat.id, timetable)
+    today = date.today()
+    day = ""
+    if message.text.lower() == "сегодня":
+        day = date.today().strftime("%d.%m")
+    elif message.text.lower() == 'завтра':
+        day = (today + timedelta(days=1)).strftime("%d.%m")
+    timetable = get_day_by_date(weeks, day)
+    bot.send_message(message.chat.id, get_beautiful_timetable(timetable))

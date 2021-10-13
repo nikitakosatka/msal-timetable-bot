649447fe4f3742864d7c596c89b91d3b88aaaaf3
nikitakosatka <nikitakosatka@yandex.ru>
2021-10-13 13:56:15 +0300

    Некоторые комментарии

--- excel_parser.py
+++ excel_parser.py
@@ -16,6 +16,7 @@ def get_weeks(file="Raspisanie.xlsx"):
 
     string_num = -1
     for week in weeks:
+        # Определение колонок в таблице
         if column == "DE":
             pass
         elif not weeks[2].is_empty() and weeks[3].is_empty():
@@ -23,16 +24,18 @@ def get_weeks(file="Raspisanie.xlsx"):
             column = "DE"
         else:
             column = "AB"
-        string_num += 3
-        for day in week.get_days():
+        string_num += 3  # Между каждой неделей расстояние 3 строки
+        for day in week.get_days():  #
             while True:
+                # Первая строка дня недели
                 if str(sheet[f"{column[0]}{string_num}"].value).split()[
-                    0] in week_names and day.get_date() == "":
+                        0] in week_names and day.get_date() == "":
                     day.set_weekday(
                         str(sheet[f"{column[0]}{string_num}"].value).split()[0].capitalize())
                     day.set_date(sheet[f"{column[0]}{string_num}"].value.split()[1])
                     string_num += 1
                     continue
+                # Если текущаяя строка является временем занятия
                 elif len(str(sheet[f"{column[0]}{string_num}"].value).split("-")) == 2:
                     lesson = Lesson(' '.join(sheet[f"{column[1]}{string_num}"].value.split()[:-2]),
                                     sheet[f"{column[0]}{string_num}"].value,
@@ -42,6 +45,7 @@ def get_weeks(file="Raspisanie.xlsx"):
                 elif day.get_date() != "":
                     break
 
+    # Расставляем недели по порядку
     weeks[1], weeks[2], weeks[3], weeks[4] = weeks[3], weeks[1], weeks[4], weeks[2]
 
     return weeks
